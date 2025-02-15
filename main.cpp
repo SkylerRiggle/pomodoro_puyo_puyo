da6757ceae2a341a27c4ffb39782af614f0e72ac
Skyler Riggle <skyler32702@gmail.com>
2025-02-15 13:30:32 -0600

    Included Raylib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <raylib/raylib.h>
 
 int main(void) {
-	printf("Wazzup!!!\n");
+	InitWindow(400, 400, "Test Window!");
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawFPS(0, 0);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
